a2cd4b97b4f988c332bc59ae9660b3a203f2be54
RealInfoProduct <reallocinfotechproduct@gmail.com>
2026-04-02 11:04:26 +0530

    app  change fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,26 +12,26 @@ export class AppComponent {
   constructor(public translate: TranslateService , private router: Router , private firebaseService : FirebaseService) {
     translate.addLangs(['en', 'fr','es','de']);
     translate.setDefaultLang('en');
-    this.logActivePath()
+    // this.logActivePath()
 
   }
 
 
-  logActivePath() {
-    this.router.events.subscribe(event => {
-      if (event instanceof NavigationEnd) {
-        this.firebaseService.getUserList().subscribe((res => {
-          if (res) {
-            const userData: any = res.find((id: any) => id.id === localStorage.getItem("userId"))
-              if (!userData?.isActive) {
-                  localStorage.clear()
-                  this.router.navigate(['/authentication/side-login']);
-              }
-          }
-        }))
-      }
-    });
-  }
+  // logActivePath() {
+  //   this.router.events.subscribe(event => {
+  //     if (event instanceof NavigationEnd) {
+  //       this.firebaseService.getUserList().subscribe((res => {
+  //         if (res) {
+  //           const userData: any = res.find((id: any) => id.id === localStorage.getItem("userId"))
+  //             if (!userData?.isActive) {
+  //                 localStorage.clear()
+  //                 this.router.navigate(['/authentication/side-login']);
+  //             }
+  //         }
+  //       }))
+  //     }
+  //   });
+  // }
 
 
   
